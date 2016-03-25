bc21525ff018cf9bec4dfcabb1de7a8ade8d1f94
Olexandr Draiuk <aladygma0@gmail.com>
2016-03-25 15:48:33 +0200

    Исправил ошибку в функции getMessage

--- build/js/check.js
+++ build/js/check.js
@@ -11,6 +11,15 @@ function getMessage(a, b) {
   } else if (typeof a == "number") {
     return "Я прыгнул на " + a * 100 + " сантиметров";
 
+  } else if ((typeof a == "object") && (typeof b == "object") ) {
+    var length = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      length += a[i] * b[i];
+    }
+
+    return "Я прошел " + length + " метров";
+
   } else if (typeof a == "object") {
     var sum = 0;
   // Може разумнее было написать отдельную функцию для подсчета элементов в массиве?
@@ -19,15 +28,6 @@ function getMessage(a, b) {
     }
 
     return "Я прошел " + sum + " шагов";
-
-  } else if (typeof a == "object" && typeof b == "object" ) {
-    var length = 0;
-
-    for (var i = 0; i < a.length; i++) {
-      length += a[i] * b[i];
-    }
-
-    return "Я прошел " + length + " метров";
   }
 
 }

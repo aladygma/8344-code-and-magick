be71e9867427ddb2cac80d544111780f646b6735
Olexandr Draiuk <aladygma0@gmail.com>
2016-03-25 15:31:40 +0200

    Задание 5 готово

---
+++ build/js/check.js
@@ -0,0 +1,32 @@
+function getMessage(a, b) {
+  if (typeof a == "boolean") {
+
+    if (a) {
+      return "Я попал в " + b;
+    } else {
+      return "Я никуда не попал";
+    }
+
+  } else if (typeof a == "number") {
+    return "Я прыгнул на " + a * 100 + " сантиметров";
+
+  } else if (typeof a == "object") {
+    var sum = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+
+    return "Я прошел " + sum + " шагов";
+
+  } else if (typeof a == "object" && typeof b == "object" ) {
+    var length = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      length += a[i] * b[i];
+    }
+
+    return "Я прошел " + length + " метров";
+  }
+
+}

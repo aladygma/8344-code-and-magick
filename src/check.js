3aab2cc84aa869b7728078f9d928256534545654
Olexandr Draiuk <aladygma0@gmail.com>
2016-04-07 16:33:39 +0300

    Генеральная уборка окончена

--- src/check.js
+++ src/check.js
@@ -2,23 +2,20 @@ function getMessage(a, b) {
   var aType = typeof a;
 
   switch (aType) {
-    case "boolean":
+    case 'boolean':
 
       if (a) {
-        return "Я попал в " + b;
+        return 'Я попал в ' + b;
       } else {
-        return "Я никуда не попал";
+        return 'Я никуда не попал';
       }
 
-      break;
-
-    case "number":
-      return "Я прыгнул на " + a * 100 + " сантиметров";
-      break;
+    case 'number':
+      return 'Я прыгнул на ' + a * 100 + ' сантиметров';
 // Саша, мне не нравится следующий участок кода.
 // Пожалуйста подскажи, если можно его улучшить
 
-    case "object":
+    case 'object':
       var aArray = Array.isArray(a);
       var bArray = Array.isArray(b);
 
@@ -29,7 +26,7 @@ function getMessage(a, b) {
           length += a[i] * b[i];
         }
 
-        return "Я прошёл " + length + " метров";
+        return 'Я прошёл ' + length + ' метров';
 
       } else if (aArray) {
         var sum = 0;
@@ -38,15 +35,14 @@ function getMessage(a, b) {
           return total + current;
         }, 0);
 
-        return "Я прошёл " + sum + " шагов";
+        return 'Я прошёл ' + sum + ' шагов';
 
-        } else {
-          return "Я сделал что-то странное"
-        }
+      } else {
+        return 'Я сделал что-то странное';
+      }
 
-      break;
 
     default:
-      alert("Я сделал что-то странное");
+      alert('Я сделал что-то странное');
   }
 }

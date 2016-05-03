52ce74a44f7a74204ea4fb3796082be4aa9b7ee7
Olexandr Draiuk <aladygma0@gmail.com>
2016-05-03 10:51:15 +0300

    Исправил ошибки

--- src/gallery.js
+++ src/gallery.js
@@ -1,91 +1,89 @@
 'use strict';
 
-module.exports = function() {
+var photoGallery = document.querySelector('.photogallery');
+var gallery = document.querySelector('.overlay-gallery');
+var galleryCloseButton = gallery.querySelector('.overlay-gallery-close');
+var galleryControlLeft = gallery.querySelector('.overlay-gallery-control-left');
+var galleryControlRight = gallery.querySelector('.overlay-gallery-control-right');
+var galleryPreview = gallery.querySelector('.overlay-gallery-preview');
+var previewNumberCurrent = gallery.querySelector('.preview-number-current');
+var previewNumberTotal = gallery.querySelector('.preview-number-total');
+var imagesAddresses = [];
+var currentImageIndex;
 
-  var IMAGES_QTY = 10;
-  var IMAGES_FIRST_NUMBER = 1;
 
-  var photoGallery = document.querySelector('.photogallery');
-  var gallery = document.querySelector('.overlay-gallery');
-  var galleryCloseButton = gallery.querySelector('.overlay-gallery-close');
-  var galleryControlLeft = gallery.querySelector('.overlay-gallery-control-left');
-  var galleryControlRight = gallery.querySelector('.overlay-gallery-control-right');
-  var galleryPreview = gallery.querySelector('.overlay-gallery-preview');
-  var previewNumberCurrent = gallery.querySelector('.preview-number-current');
-  var previewNumberTotal = gallery.querySelector('.preview-number-total');
-  var imagesAddresses = [];
-  var currentImageIndex;
+photoGallery.addEventListener('click', function(evt) {
+  if (evt.target.tagName === 'IMG') {
+    showGallery(evt);
+  }
+});
 
-  getImagesFeatures();
+function getImagesFeatures(imagesData) {
+  var domainName = 'http://localhost:8080/';
 
-  photoGallery.addEventListener('click', function(evt) {
-    if (evt.target.tagName === 'IMG') {
-      showGallery(evt);
-    }
-  });
-
-  function getImagesFeatures() {
-    for (var i = IMAGES_FIRST_NUMBER; i <= IMAGES_QTY; i++) {
-      var address = 'http://localhost:8080/img/screenshots/' + i + '.png';
-      imagesAddresses.push(address);
-    }
+  for (var i = 0; i < imagesData.length; i++) {
+    var address = domainName + imagesData[i];
+    imagesAddresses.push(address);
   }
 
+}
 
-  function showGallery(evt) {
-    gallery.classList.remove('invisible');
-    currentImageIndex = imagesAddresses.indexOf(evt.target.src);
+function showGallery(evt) {
+  gallery.classList.remove('invisible');
+  currentImageIndex = imagesAddresses.indexOf(evt.target.src);
 
-    galleryControlRight.addEventListener('click', showNextImage);
-    galleryControlLeft.addEventListener('click', showPreviousImage);
-    galleryCloseButton.addEventListener('click', hideGallery);
-    document.addEventListener('keydown', _onDocumentKeyDown);
+  galleryControlRight.addEventListener('click', showNextImage);
+  galleryControlLeft.addEventListener('click', showPreviousImage);
+  galleryCloseButton.addEventListener('click', hideGallery);
+  document.addEventListener('keydown', _onDocumentKeyDown);
 
+  showGalleryImage(currentImageIndex);
+}
+
+function _onDocumentKeyDown(evt) {
+  if (evt.keyCode === 27) {
+    hideGallery();
+  }
+}
+
+function showGalleryImage(index) {
+  previewNumberTotal.innerHTML = imagesAddresses.length;
+  previewNumberCurrent.innerHTML = parseInt(index, 10) + 1;
+
+  var previewImage = new Image();
+  previewImage.src = imagesAddresses[index];
+
+  previewImage.onload = function() {
+    galleryPreview.style.backgroundImage = 'url(' + previewImage.src + ')';
+    galleryPreview.style.width = previewImage.width + 'px';
+    galleryPreview.style.height = previewImage.height + 'px';
+  };
+}
+
+function showNextImage() {
+  if (previewNumberCurrent.innerHTML < imagesAddresses.length) {
+    currentImageIndex++;
     showGalleryImage(currentImageIndex);
   }
+}
 
-  function _onDocumentKeyDown(evt) {
-    if (evt.keyCode === 27) {
-      hideGallery();
-    }
+function showPreviousImage() {
+  if (previewNumberCurrent.innerHTML > 1) {
+    currentImageIndex--;
+    showGalleryImage(currentImageIndex);
   }
+}
 
-  function showGalleryImage(index) {
-    previewNumberTotal.innerHTML = imagesAddresses.length;
-    previewNumberCurrent.innerHTML = parseInt(index, 10) + 1;
+function hideGallery() {
+  document.removeEventListener('keydown', _onDocumentKeyDown);
+  galleryControlRight.removeEventListener('click', showNextImage);
+  galleryControlLeft.removeEventListener('click', showPreviousImage);
+  galleryCloseButton.removeEventListener('click', hideGallery);
+  gallery.classList.add('invisible');
+}
 
-    var previewImage = new Image();
-    previewImage.src = imagesAddresses[index];
+module.exports.getImagesFeatures = getImagesFeatures;
+module.exports.showGallery = showGallery;
 
-    previewImage.onload = function() {
-      galleryPreview.style.backgroundImage = 'url(' + previewImage.src + ')';
-      galleryPreview.style.width = previewImage.width + 'px';
-      galleryPreview.style.height = previewImage.height + 'px';
-    };
-  }
-
-  function showNextImage() {
-    if (previewNumberCurrent.innerHTML < imagesAddresses.length) {
-      currentImageIndex++;
-      showGalleryImage(currentImageIndex);
-    }
-  }
-
-  function showPreviousImage() {
-    if (previewNumberCurrent.innerHTML > 1) {
-      currentImageIndex--;
-      showGalleryImage(currentImageIndex);
-    }
-  }
-
-  function hideGallery() {
-    document.removeEventListener('keydown', _onDocumentKeyDown);
-    galleryControlRight.removeEventListener('click', showNextImage);
-    galleryControlLeft.removeEventListener('click', showPreviousImage);
-    galleryCloseButton.removeEventListener('click', hideGallery);
-    gallery.classList.add('invisible');
-  }
-
-};
 
 
--- src/main.js
+++ src/main.js
@@ -7,4 +7,16 @@ var gallery = require('./gallery');
 game();
 form();
 reviews();
-gallery();
+
+gallery.getImagesFeatures(
+  ['img/screenshots/1.png',
+   'img/screenshots/2.png',
+   'img/screenshots/3.png',
+   'img/screenshots/4.png',
+   'img/screenshots/5.png',
+   'img/screenshots/6.png',
+   'img/screenshots/7.png',
+   'img/screenshots/8.png',
+   'img/screenshots/9.png',
+   'img/screenshots/10.png']
+);
